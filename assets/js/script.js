bc863841ecef96b30d2e8006cfe985ed152fc4fd
sokhna.ndione <sokhnandione213@gmail.com>
2025-03-13 09:20:37 +0000

    Ajout du script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
-// document.addEventListener("DOMContentLoaded", function() {
-            //     const links = document.querySelectorAll("nav ul li a");
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("nav ul li a");
 
-            //     links.forEach(link => {
-            //         link.addEventListener("click", function(event) {
-            //             event.preventDefault();
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
 
-            //             const targetId = this.getAttribute("href").substring(1);
-            //             const targetElement = document.getElementById(targetId);
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
 
-            //             if (targetElement) {
-            //                 window.scrollTo({
-            //                     top: targetElement.offsetTop,
-            //                     behavior: "smooth"
-            //                 });
-            //             }
-            //         });
-            //     });
-            // });
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
